9ea947fc4af903398c661bd92ed0f7a4494526dc
levifleal <levifleal@Live.com>
2023-01-10 14:18:08 -0300

    Finalização do modulo 2

---
+++ src/app.js
@@ -0,0 +1,53 @@
+import express from "express";
+const app = express();
+
+app.use(express.json()); 
+
+const livros = [
+    {
+        id: 1,
+        "titulo": "O Senhor Dos Aneis"
+    },
+    {
+        id: 2,
+        "titulo": "O Hobbit"
+    }
+]
+
+app.get('/', (req, res) => {
+    res.status(200).send('curso de node');
+})
+
+app.get('/livros', (req, res) => {
+    res.status(200).json(livros);
+})
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    res.json(livros[index]);
+})
+
+app.post('/livros', (req,res) => {
+    livros.push(req.body);
+    res.status(201).send('livro adicionado com sucesso');
+});
+
+app.put('/livros/:id', (req, res) => {
+    let index = buscaLivro(req.params.id);
+    livros[index].titulo = req.body.titulo;
+    res.json(livros);
+})
+
+app.delete('/livros/:id', (req, res) => {
+    let {id} = req.params;
+    let index = buscaLivro(id);
+    livros.splice(index, 1);
+    res.send(`livro ${id} foi deletado`);
+})
+
+function findBooksById(id) {
+    return livros.findIndex(livro => livro.id === id);
+}
+
+
+export default app
